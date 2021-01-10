ac11ea0e27094aa24b2aa79002054a8736c5de2a
nsbruno <bruno.sn@live.com>
2021-01-10 10:08:52 -0300

    first commit

---
+++ DDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
